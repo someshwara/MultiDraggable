ccfa9fc1343aa815e6a850db185da4bac64bdff3
Sudheer Someshwara <sudheer.someshwara@gmail.com>
2012-01-28 14:55:36 -0500

    Final Version

---
+++ test.js
@@ -0,0 +1,7 @@
+$('document').ready(function(){
+
+
+ $(".d").multiDraggable({ group: $(".d"), dragNative : function () {}});
+ $(".d2").multiDraggable({ group: $(".d2"), dragNative : function () {}});
+
+});
